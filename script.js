f79e935b5df74558f9e926f2a446b8ecea71382b
Sidd Bairan <bairansa@gmail.com>
2021-06-29 22:03:53 +0800

    Fix spacing for readibility.

--- script.js
+++ script.js
@@ -1,85 +1,13 @@
 let playerScore = 0;
 let computerScore = 0;
-
-<<<<<<< HEAD
-function computerPlay() {
-    let selection = ["rock", "paper", "scissors"];
-    return selection[Math.floor(Math.random() * selection.length)];
-}
-
-function playerPlay() {
-    let playerPick = prompt("Rock, Paper, or Scissors?");
-    return playerPick.toLowerCase();
-}
-
-// function playRound(playerSelection, computerSelection) {
-
-//     if (playerSelection === computerSelection) {
-//         console.log("Tie!");
-//     } else if (
-//         (playerSelection === "rock" && computerSelection === "scissors") || 
-//         (playerSelection === "paper" && computerSelection === "rock") || 
-//         (playerSelection === "scissors" && computerSelection === "paper")
-//     ) {
-//         console.log(`You won, ${playerSelection} beats ${computerSelection}.`)
-//     } else if (
-//         (playerSelection === "scissors" && computerSelection === "rock") || 
-//         (playerSelection === "rock" && computerSelection === "paper") || 
-//         (playerSelection === "paper" && computerSelection === "scissors")
-//     ) {
-//         console.log(`You lost, ${playerSelection} beats ${computerSelection}.`)
-//     }
-// }
-
-// console.log(playRound(playerPlay(), computerPlay()));
-
-function game() {
-
-    function playRound(playerSelection, computerSelection) {
-
-        if (playerSelection === computerSelection) {
-            console.log("Tie!");
-        } else if (
-            (playerSelection === "rock" && computerSelection === "scissors") ||
-            (playerSelection === "paper" && computerSelection === "rock") ||
-            (playerSelection === "scissors" && computerSelection === "paper")
-        ) {
-            console.log(`You won, ${playerSelection} beats ${computerSelection}.`);
-            playerScore++;
-        } else if (
-            (playerSelection === "scissors" && computerSelection === "rock") ||
-            (playerSelection === "rock" && computerSelection === "paper") ||
-            (playerSelection === "paper" && computerSelection === "scissors")
-        ) {
-            console.log(`You lost, ${playerSelection} beats ${computerSelection}.`);
-            computerScore++;
-        }
-    }
-
-    while (playerScore <= 5 || computerScore <= 5) {
-        playRound(playerPlay(),computerPlay());
-        if (playerScore === 5) {
-            alert("Congratulations, You Won!");
-            playerScore = 0;
-            computerScore = 0;
-        } else if (computerScore === 5) {
-            alert("Yikes, better luck next time.");
-            playerScore = 0;
-            computerScore = 0;
-        }
-    }
-}
-
-game()
-=======
 const message = document.querySelector('#message');
 const choices = document.querySelectorAll('.choice');
 
 choices.forEach(choice => {
-    choice.addEventListener('click', function() {
+    choice.addEventListener('click', function () {
         playRound(choice.value);
     });
-})
+});
 
 function disableChoices() {
     choices.forEach(choice => {
@@ -109,7 +37,7 @@ function playRound(playerSelection) {
     ) {
         message.textContent = `You won, ${playerSelection} beats ${computerSelection}.`;
         playerScore++;
-       
+
         if (playerScore === 5) {
             disableChoices();
             message.textContent = "Victory!";
@@ -130,4 +58,3 @@ function playRound(playerSelection) {
     document.getElementById('playerScore').textContent = playerScore;
     document.getElementById('computerScore').textContent = computerScore;
 }
->>>>>>> rps-ui
