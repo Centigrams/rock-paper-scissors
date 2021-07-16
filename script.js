485291345184c5ca610b7c8bec1ce0bcd21b623f
Sidd Bairan <bairansa@gmail.com>
2021-07-16 19:01:48 +0800

    Create reset button and replace choice buttons to images.

--- script.js
+++ script.js
@@ -5,18 +5,48 @@ const choices = document.querySelectorAll('.choice');
 
 choices.forEach(choice => {
     choice.addEventListener('click', function () {
-        playRound(choice.value);
+        playRound(choice.alt);
     });
 });
 
 function disableChoices() {
+    /* The first version uses the value attribute of the buttons
+    to pass the playerSelection parameter. Here, the alt attribute from 
+    the images can be used as a parameter. */
     choices.forEach(choice => {
-        choice.disabled = true;
+        choice.alt = null;
+    });
+}
+
+function enableChoices() {
+    /* Since disableChoices() nulls the alt attribute, enableChoices()
+    reverts the attribute to make the game restart.*/
+    choices.forEach(choice => {
+        if (choice.id === "rock") choice.alt = 'rock';
+        if (choice.id === "paper") choice.alt = 'paper';
+        if (choice.id === "scissors") choice.alt = 'scissors';
     });
 }
 
 function reset() {
-    window.location.reload();
+    playerScore = 0;
+    computerScore = 0;
+    document.getElementById('playerScore').textContent = playerScore;
+    document.getElementById('computerScore').textContent = computerScore;
+    enableChoices();
+}
+
+function resetButtonActivate() {
+    if (playerScore === 5) {
+        buttonMessage = 'Victory!'
+    } else if (computerScore === 5) {
+        buttonMessage = 'Defeat!'
+    }
+    const resetButton = document.createElement('button');
+    resetButton.setAttribute('class', 'reset-button')
+    resetButton.textContent = `${buttonMessage} Click here to restart.`;
+    message.appendChild(resetButton);
+    resetButton.addEventListener('click', reset);
 }
 
 function computerPlay() {
@@ -40,7 +70,7 @@ function playRound(playerSelection) {
 
         if (playerScore === 5) {
             disableChoices();
-            message.textContent = "Victory!";
+            resetButtonActivate();
         }
     } else if (
         (playerSelection === "scissors" && computerSelection === "rock") ||
@@ -52,7 +82,7 @@ function playRound(playerSelection) {
 
         if (computerScore === 5) {
             disableChoices();
-            message.textContent = "Defeat!";
+            resetButtonActivate();
         }
     }
     document.getElementById('playerScore').textContent = playerScore;
