05f0a4e6b900d7390a34dc9f05eac5a562967c0c
Sidd Bairan <bairansa@gmail.com>
2021-08-16 17:19:35 +0800

    Reformat code according to airbnb's styling

--- script.js
+++ script.js
@@ -3,89 +3,90 @@ let computerScore = 0;
 const message = document.querySelector('#message');
 const choices = document.querySelectorAll('.choice');
 
-choices.forEach(choice => {
-    choice.addEventListener('click', function () {
-        playRound(choice.alt);
-    });
-});
-
 function disableChoices() {
-    /* The first version uses the value attribute of the buttons
-    to pass the playerSelection parameter. Here, the alt attribute from 
+  /* The first version uses the value attribute of the buttons
+    to pass the playerSelection parameter. Here, the alt attribute from
     the images can be used as a parameter. */
-    choices.forEach(choice => {
-        choice.alt = null;
-    });
+  choices.forEach((choice) => {
+    const playerChoice = choice;
+    playerChoice.alt = null;
+  });
 }
 
 function enableChoices() {
-    /* Since disableChoices() nulls the alt attribute, enableChoices()
-    reverts the attribute to make the game restart.*/
-    choices.forEach(choice => {
-        if (choice.id === "rock") choice.alt = 'rock';
-        if (choice.id === "paper") choice.alt = 'paper';
-        if (choice.id === "scissors") choice.alt = 'scissors';
-    });
+  /* Since disableChoices() nulls the alt attribute, enableChoices()
+    reverts the attribute to make the game restart. */
+  choices.forEach((choice) => {
+    const gameChoice = choice;
+    if (choice.id === 'rock') gameChoice.alt = 'rock';
+    if (choice.id === 'paper') gameChoice.alt = 'paper';
+    if (choice.id === 'scissors') gameChoice.alt = 'scissors';
+  });
 }
 
 function reset() {
-    playerScore = 0;
-    computerScore = 0;
-    document.getElementById('playerScore').textContent = playerScore;
-    document.getElementById('computerScore').textContent = computerScore;
-    enableChoices();
+  playerScore = 0;
+  computerScore = 0;
+  document.getElementById('playerScore').textContent = playerScore;
+  document.getElementById('computerScore').textContent = computerScore;
+  enableChoices();
 }
 
 function resetButtonActivate() {
-    let buttonMessage;
-    if (playerScore === 5) {
-        buttonMessage = 'Victory!'
-    } else if (computerScore === 5) {
-        buttonMessage = 'Defeat!'
-    }
-    const resetButton = document.createElement('button');
-    resetButton.setAttribute('class', 'reset-button')
-    resetButton.textContent = `${buttonMessage} Click here to restart.`;
-    message.appendChild(resetButton);
-    resetButton.addEventListener('click', reset);
+  let buttonMessage;
+  if (playerScore === 5) {
+    buttonMessage = 'Victory!';
+  } else if (computerScore === 5) {
+    buttonMessage = 'Defeat!';
+  }
+  const resetButton = document.createElement('button');
+  resetButton.setAttribute('class', 'reset-button');
+  resetButton.textContent = `${buttonMessage} Click here to restart.`;
+  message.appendChild(resetButton);
+  resetButton.addEventListener('click', reset);
 }
 
 function computerPlay() {
-    let selection = ["rock", "paper", "scissors"];
-    return selection[Math.floor(Math.random() * selection.length)];
+  const selection = ['rock', 'paper', 'scissors'];
+  return selection[Math.floor(Math.random() * selection.length)];
 }
 
 function playRound(playerSelection) {
+  const computerSelection = computerPlay();
 
-    let computerSelection = computerPlay();
+  if (playerSelection === computerSelection) {
+    message.textContent = "It's a tie!";
+  } else if (
+    (playerSelection === 'rock' && computerSelection === 'scissors')
+        || (playerSelection === 'paper' && computerSelection === 'rock')
+        || (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    message.textContent = `You won, ${playerSelection} beats ${computerSelection}.`;
+    playerScore += 1;
 
-    if (playerSelection === computerSelection) {
-        message.textContent = "It's a tie!";
-    } else if (
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "paper" && computerSelection === "rock") ||
-        (playerSelection === "scissors" && computerSelection === "paper")
-    ) {
-        message.textContent = `You won, ${playerSelection} beats ${computerSelection}.`;
-        playerScore++;
-
-        if (playerScore === 5) {
-            disableChoices();
-            resetButtonActivate();
-        }
-    } else if (
-        (playerSelection === "scissors" && computerSelection === "rock") ||
-        (playerSelection === "rock" && computerSelection === "paper") ||
-        (playerSelection === "paper" && computerSelection === "scissors")
-    ) {
-        message.textContent = `You lost, ${playerSelection} beats ${computerSelection}.`;
-        computerScore++;
-
-        if (computerScore === 5) {
-            disableChoices();
-            resetButtonActivate();
-        }
+    if (playerScore === 5) {
+      disableChoices();
+      resetButtonActivate();
     }
-    document.getElementById('playerScore').textContent = playerScore;
-    document.getElementById('computerScore').textContent = computerScore;
+  } else if (
+    (playerSelection === 'scissors' && computerSelection === 'rock')
+        || (playerSelection === 'rock' && computerSelection === 'paper')
+        || (playerSelection === 'paper' && computerSelection === 'scissors')
+  ) {
+    message.textContent = `You lost, ${playerSelection} beats ${computerSelection}.`;
+    computerScore += 1;
+
+    if (computerScore === 5) {
+      disableChoices();
+      resetButtonActivate();
+    }
+  }
+  document.getElementById('playerScore').textContent = playerScore;
+  document.getElementById('computerScore').textContent = computerScore;
 }
+
+choices.forEach((choice) => {
+  choice.addEventListener('click', () => {
+    playRound(choice.alt);
+  });
+});
